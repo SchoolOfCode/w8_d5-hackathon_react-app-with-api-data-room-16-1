ed1b7ff784823d40afff183f6168b60c830853e2
jafcoder <joefarr113@gmail.com>
2022-01-14 11:01:03 +0000

    higher and lower buttons, card generated on page render

--- src/Components/CurrentCard/index.js
+++ src/Components/CurrentCard/index.js
@@ -1,24 +1,42 @@
 import { useState, useEffect } from "react";
 
+import Button from "../Button";
+
 function CurrentCard({ deckID }) {
   const [card, setCard] = useState("");
+  const [cardImage, setCardImage] = useState("");
+  const [cardValue, setCardValue] = useState(0);
   useEffect(() => {
-    async function getCurrentCard() {
+    async function getFirstCard() {
       const response = await fetch(
         `http://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
       );
       const data = await response.json();
-      console.log(data);
+      //   console.log(data);
       setCard(data);
+      setCardImage(data.cards.image);
+
+      //   console.log(cardImage);
     }
-    getCurrentCard();
+    getFirstCard();
   }, [deckID]);
+
+  async function getNewCard() {
+    const response = await fetch(
+      `http://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+    );
+    const data = await response.json();
+    console.log(data);
+    setCard(data);
+    setCardImage(data.cards.image);
+  }
 
   return (
     <div>
       <p>{deckID}</p>
-      {/* <img src={card[0].image}></img> */}
-      <p>{card[0].suit}</p>
+      {cardImage !== "" && <img src={card.cards[0].image} alt={"card"}></img>}
+      <Button text="higher" />
+      <Button text="lower" />
     </div>
   );
 }
